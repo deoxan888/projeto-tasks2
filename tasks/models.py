0b93df29c71b6d21ece2bdfb40f470d446d54329
deoxan888 <66044047+deoxan888@users.noreply.github.com>
2020-11-11 13:26:30 +0000

    #09

--- tasks/models.py
+++ tasks/models.py
@@ -11,3 +11,6 @@ class Task(models.Model):
     done = models.CharField(max_length=5, choices=STATUS,)
     created_at = models.DateTimeField(auto_now_add=True)                      
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title;
