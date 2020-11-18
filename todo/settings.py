edd1203469d4d87fd37c46acf1991f418ac5bfeb
deoxan888 <66044047+deoxan888@users.noreply.github.com>
2020-11-18 13:26:01 +0000

    #14

--- todo/settings.py
+++ todo/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'tasks',
     'about',
     'users',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
 )
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
